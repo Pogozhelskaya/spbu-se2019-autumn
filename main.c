d85638172fecd0718cbd0b27d5d8b5dd873c5209
Vlada Pogozhelskaya <31728695+Pogozhelskaya@users.noreply.github.com>
2019-12-15 22:38:47 +0300

    Fixed main.c

--- main.c
+++ main.c
@@ -1,220 +1,134 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled0.ipynb",
-      "provenance": [],
-      "collapsed_sections": []
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "accelerator": "GPU"
-  },
-  "cells": [
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "r2qSNe1v9t8R",
-        "colab_type": "code",
-        "outputId": "f34fb4de-9cc3-4633-e7f1-f3c8f7afc676",
-        "colab": {
-          "base_uri": "https://localhost:8080/",
-          "height": 107
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+#include <ctime>
+
+#define THREADS 256
+#define BLOCKS 32768
+#define NUM THREADS * BLOCKS
+
+bool check(int *array, int length) {
+	bool result = true;
+	for (int i = 0; i < length - 1; i++) {
+		if (array[i] > array[i + 1]) {
+		    return false;
+		}
+	}
+	return result;
+}
+
+void bitonic(int* array, int length) {
+	int K = log2(length);
+	int d = 1 << K;
+    K--;
+	for (int n = 0; n < d >> 1; n++) {
+        if (array[n] > array[d - n - 1]) {
+            int temp = array[n];
+            array[n] = array[d - n - 1];
+            array[d - n - 1] = temp;
         }
-      },
-      "source": [
-        "%%cu\n",
-        "#include \"cuda_runtime.h\"\n",
-        "#include \"device_launch_parameters.h\"\n",
-        "#include <stdio.h>\n",
-        "#include <stdlib.h>\n",
-        "#include <cmath>\n",
-        "#include <ctime>\n",
-        "\n",
-        "#define THREADS 256\n",
-        "#define BLOCKS 32768\n",
-        "#define NUM THREADS * BLOCKS\n",
-        "\n",
-        "bool check(int *array, int length) {\n",
-        "\tbool result = true;\n",
-        "\tfor (int i = 0; i < length - 1; i++) {\n",
-        "\t\tif (array[i] > array[i + 1]) {\n",
-        "\t\t    return false;\n",
-        "\t\t}\n",
-        "\t}\n",
-        "\treturn result;\n",
-        "}\n",
-        "\n",
-        "void bitonic(int* array, int length) {\n",
-        "\tint K = log2(length);\n",
-        "\tint d = 1 << K;\n",
-        "    K--;\n",
-        "\tfor (int n = 0; n < d >> 1; n++) {\n",
-        "        if (array[n] > array[d - n - 1]) {\n",
-        "            int temp = array[n];\n",
-        "            array[n] = array[d - n - 1];\n",
-        "            array[d - n - 1] = temp;\n",
-        "        }\n",
-        "    }\n",
-        "\tfor (int k = K; k > 0; k--) {\n",
-        "\t\td = 1 << k;\n",
-        "\t\tfor (int m = 0; m < length; m += d) {\n",
-        "\t\t    for (int n = 0; n < d >> 1; n++) {\n",
-        "                if (array[m + n] > array[m + (d >> 1) + n]) {\n",
-        "                    int temp = array[m + n];\n",
-        "                    array[m + n] = array[m + (d >> 1) + n];\n",
-        "                    array[m + (d >> 1) + n] = temp;\n",
-        "                }\n",
-        "            }\n",
-        "        }\n",
-        "\t}\n",
-        "}\n",
-        "\n",
-        "void bitonic_sort(int* array, int length) {\n",
-        "    int* arr = (int*)malloc(sizeof(int) * length);\n",
-        "\tfor (int n = 0; n < length; n++)\n",
-        "\t\tarr[n] = array[n];\n",
-        "\tint K = log2(length);\n",
-        "\tfor (int k = 1; k <= K; k++) {\n",
-        "\t\tfor (int n = 0; n < length; n += 1 << k) {\n",
-        "            int* arr_ptr = &arr[n];\n",
-        "\t\t\tbitonic(arr_ptr, 1 << k);\n",
-        "\t\t}\n",
-        "\t}\n",
-        "\tfor (int n = 0; n < length; n++) {\n",
-        "\t\tarray[n] = arr[n];\n",
-        "\t}\n",
-        "\tfree(arr);\n",
-        "}\n",
-        "\n",
-        "__global__ void bitonic_gpu(int *array, int j, int k) {\n",
-        "    int i, ixj;\n",
-        "    i = threadIdx.x + blockDim.x * blockIdx.x;\n",
-        "    ixj = i^j;\n",
-        "    if ((ixj)>i) {\n",
-        "        if ((i&k)==0) {\n",
-        "            if (array[i] > array[ixj]) {\n",
-        "                int temp = array[i];\n",
-        "                array[i] = array[ixj];\n",
-        "                array[ixj] = temp;\n",
-        "            }\n",
-        "        }\n",
-        "        if ((i&k)!=0) {\n",
-        "            if (array[i] < array[ixj]) {\n",
-        "                int temp = array[i];\n",
-        "                array[i] = array[ixj];\n",
-        "                array[ixj] = temp;\n",
-        "            }\n",
-        "        }\n",
-        "    } \n",
-        "}\n",
-        "\n",
-        "void bitonic_sort_gpu(int* array, int length) {\n",
-        "    int* gpu_array;\n",
-        "    cudaMalloc(&gpu_array, length);\n",
-        "    cudaMemcpy(gpu_array, array, length, cudaMemcpyHostToDevice);\n",
-        "    dim3 num_blocks(BLOCKS, 1);\n",
-        "\tdim3 num_threads_in_block(THREADS, 1);\n",
-        "    for (int k = 2; k <= length; k <<= 1) {\n",
-        "\t\tfor (int j = k >> 1; j > 0; j >>= 1) {\n",
-        "\t\t\tbitonic_gpu <<< num_blocks, num_threads_in_block >>> (gpu_array, j, k);\n",
-        "\t\t}\n",
-        "\t}\n",
-        "    cudaMemcpy(array, gpu_array, length, cudaMemcpyDeviceToHost);\n",
-        "    cudaFree(gpu_array);\n",
-        "}\n",
-        "\n",
-        "int main(int argc, char **argv) {\n",
-        "    int length = NUM;\n",
-        "\tint *array = (int*)malloc(sizeof(int) * length);\n",
-        "    int *array_gpu = (int*)malloc(sizeof(int) * length);\n",
-        "\n",
-        "    srand(time(0));\n",
-        "\tfor (int i = 0; i < length; i++) {\n",
-        "\t\tarray[i] = rand() % 10000;\n",
-        "    }\n",
-        "\n",
-        "    time_t start = clock();\n",
-        "    bitonic_sort(array, length);\n",
-        "    time_t stop = clock();\n",
-        "    double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;\n",
-        "\n",
-        "    if (!check(array, length)) {\n",
-        "\t\tprintf(\"Cpu check failed\\n\");\n",
-        "\t} else {\n",
-        "          printf(\"Check successed\\nElapsed time on cpu: %.3fs\\n\", elapsed);\n",
-        "    }\n",
-        "\n",
-        "    memcpy(array_gpu, array, sizeof(int) * length);\n",
-        "    start = clock();\n",
-        "    bitonic_sort_gpu(array_gpu, length);\n",
-        "    stop = clock();\n",
-        "    elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;\n",
-        "    if (!check(array_gpu, length)) {\n",
-        "\t\tprintf(\"Gpu check failed\\n\");\n",
-        "\t} else {\n",
-        "          printf(\"Check cuccessed\\nElapsed time on cpu: %.3fs\\n\", elapsed);\n",
-        "    }\n",
-        "    free(array);\n",
-        "    return 0;\n",
-        "}"
-      ],
-      "execution_count": 61,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "text": [
-            "Check successed\n",
-            "Elapsed time on cpu: 7.004s\n",
-            "Check cuccessed\n",
-            "Elapsed time on cpu: 0.102s\n",
-            "\n"
-          ],
-          "name": "stdout"
-        }
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "qMRzjo89tCsW",
-        "colab_type": "code",
-        "colab": {}
-      },
-      "source": [
-        "!ls \"src/\""
-      ],
-      "execution_count": 0,
-      "outputs": []
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "vDRsy4FIRuQC",
-        "colab_type": "code",
-        "outputId": "ce3e6e5c-55fb-489d-9eef-3fe6a110eec4",
-        "colab": {
-          "base_uri": "https://localhost:8080/",
-          "height": 53
-        }
-      },
-      "source": [
-        "%load_ext nvcc_plugin"
-      ],
-      "execution_count": 58,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "text": [
-            "The nvcc_plugin extension is already loaded. To reload it, use:\n",
-            "  %reload_ext nvcc_plugin\n"
-          ],
-          "name": "stdout"
-        }
-      ]
     }
-  ]
+	for (int k = K; k > 0; k--) {
+		d = 1 << k;
+		for (int m = 0; m < length; m += d) {
+		    for (int n = 0; n < d >> 1; n++) {
+                if (array[m + n] > array[m + (d >> 1) + n]) {
+                    int temp = array[m + n];
+                    array[m + n] = array[m + (d >> 1) + n];
+                    array[m + (d >> 1) + n] = temp;
+                }
+            }
+        }
+	}
+}
+
+void bitonic_sort(int* array, int length) {
+    int* arr = (int*)malloc(sizeof(int) * length);
+	for (int n = 0; n < length; n++)
+		arr[n] = array[n];
+	int K = log2(length);
+	for (int k = 1; k <= K; k++) {
+		for (int n = 0; n < length; n += 1 << k) {
+            int* arr_ptr = &arr[n];
+			bitonic(arr_ptr, 1 << k);
+		}
+	}
+	for (int n = 0; n < length; n++) {
+		array[n] = arr[n];
+	}
+	free(arr);
+}
+
+__global__ void bitonic_gpu(int *array, int j, int k) {
+    int i, ixj;
+    i = threadIdx.x + blockDim.x * blockIdx.x;
+    ixj = i^j;
+    if ((ixj)>i) {
+        if ((i&k)==0) {
+            if (array[i] > array[ixj]) {
+                int temp = array[i];
+                array[i] = array[ixj];
+                array[ixj] = temp;
+            }
+        }
+        if ((i&k)!=0) {
+            if (array[i] < array[ixj]) {
+                int temp = array[i];
+                array[i] = array[ixj];
+                array[ixj] = temp;
+            }
+        }
+    } 
+}
+
+void bitonic_sort_gpu(int* array, int length) {
+    int* gpu_array;
+    cudaMalloc(&gpu_array, length);
+    cudaMemcpy(gpu_array, array, length, cudaMemcpyHostToDevice);
+    dim3 num_blocks(BLOCKS, 1);
+	dim3 num_threads_in_block(THREADS, 1);
+    for (int k = 2; k <= length; k <<= 1) {
+		for (int j = k >> 1; j > 0; j >>= 1) {
+			bitonic_gpu <<< num_blocks, num_threads_in_block >>> (gpu_array, j, k);
+		}
+	}
+    cudaMemcpy(array, gpu_array, length, cudaMemcpyDeviceToHost);
+    cudaFree(gpu_array);
+}
+
+int main(int argc, char **argv) {
+    int length = NUM;
+	int *array = (int*)malloc(sizeof(int) * length);
+    int *array_gpu = (int*)malloc(sizeof(int) * length);
+
+    srand(time(0));
+	for (int i = 0; i < length; i++) {
+		array[i] = rand() % 10000;
+    }
+
+    time_t start = clock();
+    bitonic_sort(array, length);
+    time_t stop = clock();
+    double elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
+
+    if (!check(array, length)) {
+		printf("Cpu check failed\n");
+	} else {
+          printf("Check successed\nElapsed time on cpu: %.3fs\n", elapsed);
+    }
+
+    memcpy(array_gpu, array, sizeof(int) * length);
+    start = clock();
+    bitonic_sort_gpu(array_gpu, length);
+    stop = clock();
+    elapsed = ((double) (stop - start)) / CLOCKS_PER_SEC;
+    if (!check(array_gpu, length)) {
+		printf("Gpu check failed\n");
+	} else {
+          printf("Check cuccessed\nElapsed time on cpu: %.3fs\n", elapsed);
+    }
+    free(array);
+    return 0;
 }
